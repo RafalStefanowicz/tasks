17906cefe9897a8b2a6d8ad63316e09dde769e42
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-08-14 16:07:52 +0200

    Updates

---
+++ precache-manifest.d75e5b4dccb66c66a55cf84290004480.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a73c07f36a2a70bb35dc53407248ae6",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "37b4d978928d04ed1368",
+    "url": "/tasks/static/js/2.c1b6fb32.chunk.js"
+  },
+  {
+    "revision": "8df9e530a55e99cd42c3",
+    "url": "/tasks/static/js/main.dfe4f517.chunk.js"
+  },
+  {
+    "revision": "7d8089a231448516824e",
+    "url": "/tasks/static/js/runtime~main.b1405a7c.js"
+  }
+]);
