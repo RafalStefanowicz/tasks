cd4a8272c6dd09ab3a861d0eb0cfda1c549d4d8e
Rafal Stefanowicz <rafalstefanowicz91@gmail.com>
2019-08-15 13:45:45 +0200

    Updates

--- precache-manifest.ebef513ca7ce3cfb6f29399441b05fe6.js
+++ precache-manifest.ebef513ca7ce3cfb6f29399441b05fe6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6c820610f22456da5f82bd293f886251",
+    "revision": "544068a28c46dd6e1474a6bdfde85100",
     "url": "/tasks/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tasks/static/js/2.c1b6fb32.chunk.js"
   },
   {
-    "revision": "0a96509f75a883947e09",
-    "url": "/tasks/static/js/main.7e99b10c.chunk.js"
+    "revision": "895d0cb5912d409c553e",
+    "url": "/tasks/static/js/main.109c7fd4.chunk.js"
   },
   {
     "revision": "7d8089a231448516824e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tasks/precache-manifest.751a786b2cf7eb510c27f79d04910512.js"
+  "/tasks/precache-manifest.ebef513ca7ce3cfb6f29399441b05fe6.js"
 );
 
 self.addEventListener('message', (event) => {
